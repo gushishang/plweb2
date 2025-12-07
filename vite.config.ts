768132c5e5a96ea15f3ed1b837c927e4ef8f4cec
小临 <155876693+wsxiaolin@users.noreply.github.com>
2025-12-07 17:32:08 +0800

    feat: add PWA support with vite-plugin-pwa and update assets (#5)
    - Added vite-plugin-pwa to enable Progressive Web App features.
    - Created and configured PWA manifest with app details and icons.
    - Added mobile and desktop screenshots for better user experience.
    - Included new asset files: desktop.png, logo-192.png, logo-512.png, and mobile.png.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,55 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      devOptions: {
+        enabled: true,
+      },
+
+      manifest: {
+        name: "Physics-Lab-Web",
+        short_name: "plweb",
+        description: "Physics Lab Web - Online community",
+        theme_color: "#3397e9ff",
+        icons: [
+          {
+            src: "/assets/icons/logo-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/assets/icons/logo-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        screenshots: [
+          {
+            src: "/assets/mobile.png",
+            sizes: "818x1339",
+            type: "image/png",
+          },
+          {
+            src: "/assets/desktop.png",
+            sizes: "1033x698",
+            type: "image/png",
+            form_factor: "wide",
+          },
+        ],
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@popup": "/src/services/popup",
